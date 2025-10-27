061eda35886f830506ca476468e3bcf057ecbeb3
TuyuriTio <tuyuritio@gmail.com>
2025-10-28 00:53:56 +0900

    feat: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Page
+
+Built with [Astro](https://astro.build/). : )
---
+++ astro.config.ts
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import unocss from "unocss/astro";
+import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		unocss({ injectReset: true }),
+		svelte(),
+		icon(),
+	]
+});
